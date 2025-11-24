0fdeba0657d7c267585b40a66b578d592c88341d
ryan <rsuh@berkeley.edu>
2025-11-23 23:40:26 -0800

    initial variables

--- proj5/src/core/Main.java
+++ proj5/src/core/Main.java
@@ -1,9 +1,9 @@
 package core;
 
+import tileengine.TETile;
+
 public class Main {
     public static void main(String[] args) {
-
-        // build your own world!
 
 
     }
--- proj5/src/core/World.java
+++ proj5/src/core/World.java
@@ -1,7 +1,20 @@
 package core;
 
+import tileengine.TETile;
+
+import java.util.Random;
+
 public class World {
 
-    // build your own world!
+    private TETile[][] ryanworld;
+    private int wide;
+    private int high;
+    private Random jdong;
+    public World(long seed) {
+        //wide =
+        //high =
+        jdong = new Random(seed);
+        //ryanworld = new TETile[wide][high]
+    }
 
 }
