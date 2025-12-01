0beb5c284a8e37681c21ce16d8e83b3fc61c35e8
ryan <rsuh@berkeley.edu>
2025-12-01 01:24:22 -0800

    fixed gui

--- proj5/src/core/Main.java
+++ proj5/src/core/Main.java
@@ -37,6 +37,7 @@ public class Main {
             }
             if (StdDraw.hasNextKeyTyped()) {
                 move(StdDraw.nextKeyTyped());
+                hud(mousex, mousey);
             }
 
         }
